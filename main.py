43e467cac56d3fa40fff4ee9b10760b4ef27b0f7
fortu <david.orourke1990@gmail.com>
2021-04-18 15:49:55 +0100

    *First Commit*

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("MLS19.csv")
+
+##print the head
+print(dataset.head())
+
+### check for missing values
+print(dataset.isnull().sum())
