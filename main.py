a4f3229e174877517db78ee0e5e28d1c6e6a5a99
fortu <david.orourke1990@gmail.com>
2021-04-22 23:23:09 +0100

    *Third Commit*

--- main.py
+++ main.py
@@ -3,10 +3,42 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv("MLS17.csv")
+MLS19 = pd.read_csv("MLS19.csv")
 
 ##print the head
-print(dataset.head())
+print(MLS19.head())
 
-### check for missing values
-print(dataset.isnull().sum())
+##print the tail
+print(MLS19.tail())
+
+##get the shape of dataset
+MLS19.shape
+print(MLS19.shape)
+
+##show the dataframe info
+MLS19.info()
+print(MLS19.info())
+
+##show the columns
+MLS19.columns
+print(MLS19.columns)
+
+##show the index
+MLS19.index
+print(MLS19.index)
+
+##describe the dataset
+MLS19.describe()
+print(MLS19.describe())
+
+##if i wanted to sort by clubs in alphabetical order
+MLS19club = MLS19.sort_values("Club")
+print(MLS19club)
+
+##sort my multiple variables
+MLS19Aplhabet = MLS19.sort_values(["Club", "First_Name"])
+print(MLS19Aplhabet)
+
+#if i wanted to sort by salary
+MLS19_Salary = MLS19.sort_values("Salary", ascending=False)
+print(MLS19_Salary.head())
